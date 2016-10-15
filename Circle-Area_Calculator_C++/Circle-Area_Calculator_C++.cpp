1f2b6796cb5e4369251efcc80b927b3d4d2f0451
AbuGitHub <abuzar.noorali@hotmail.com>
2016-10-15 18:25:21 -0500

    Update Circle-Area_Calculator_C++.cpp

--- Circle-Area_Calculator_C++/Circle-Area_Calculator_C++.cpp
+++ Circle-Area_Calculator_C++/Circle-Area_Calculator_C++.cpp
@@ -1,7 +1,5 @@
-/*  Abuzar Noorali
-Assignment: 1, Ch 2
-Due Date: 09/06/2016
-Purpose/Description: This program calculates the diameter, circumference, and area of a circle with a given radius.
+/*  Author: Abuzar Noorali
+    Purpose/Description: This program calculates the diameter, circumference, and area of a circle with a given radius.
 */
 #include <iostream>
 using namespace std;
@@ -9,11 +7,13 @@ int main()
 {
 	int radius = 0;
 	cout << "Please enter the radius of a circle\n"; //display message
-	cin >> radius; //redefine radius variable
-				   //Lines 11-13 calculate and output diameter, circumference, and area based on radius input by user.
+	cin >> radius; 					 //redefine radius variable
+	
+	//Lines 11-13 calculate and output diameter, circumference, and area based on radius input by user.
+	
 	cout << "The Diameter of the circle is: " << (2 * radius);
 	cout << "\nThe Circumference of the circle is: " << (2 * 3.14159 * radius);
 	cout << "\nThe Area of the circle is: " << (3.14159 * (radius * radius));
-	cout << "\n"; //Space for display
-	return 0; // indicate that program ended successfully
-} //end of main function
+	cout << "\n"; 					//Space for display
+	return 0; 					//indicate that program ended successfully
+}							//End of main function
